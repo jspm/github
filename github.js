87a8024574d85b3546bb48be1843cbef4ad3a78d
guybedford <guybedford@gmail.com>
2013-10-04 11:49:31 +0200

    github release support with test

--- github.js
+++ github.js
@@ -3,8 +3,12 @@ var fs = require('fs');
 var path = require('path');
 var mkdirp = require('mkdirp');
 var rimraf = require('rimraf');
-//var githubAPI = require('github');
-//var github = new githubAPI({ version: '3.0.0' });
+var githubAPI = require('github');
+var github = new githubAPI({ version: '3.0.0' });
+var request = require('request');
+
+var tar = require('tar');
+var zlib = require('zlib');
 
 var execOpt;
 
@@ -45,21 +49,61 @@ var touchRepo = function(repo, callback, errback) {
   });
 }
 
-var prepDir = function(dir, callback) {
+var clearDir = function(dir, callback) {
   fs.exists(dir, function(exists) {
 
     (exists ? rimraf : function(dir, callback) { callback(); })(dir, function(err) {
       if (err)
         return callback(err);
 
-      mkdirp(dir, function(err) {
-        if (err)
-          return callback(err);
-
-        callback();
-      });
-
+      callback();
     });
+  });
+}
+
+var prepDir = function(dir, callback) {
+  clearDir(dir, function(err) {
+    if (err)
+      return callback(err);
+    mkdirp(dir, function(err) {
+      if (err)
+        return callback(err);
+
+      callback();
+    });
+
+  });
+}
+
+var checkReleases = function(repo, version, hasRelease, noRelease, errback) {
+  github.repos.listReleases({
+    user: repo.split('/')[0],
+    repo: repo.split('/')[1]
+  }, function(err, res) {
+    if (err)
+      return errback();
+
+    // run through releases list to see if we have this version tag
+    for (var i = 0; i < res.length; i++) {
+      if (res[i].tag_name == version) {
+        var firstAsset = res[i].assets[0];
+        if (!firstAsset)
+          return noRelease();
+
+        var assetType;
+
+        if (firstAsset.name.substr(firstAsset.name.length - 7, 7) == '.tar.gz' || firstAsset.name.substr(firstAsset.name.length - 4, 4) == '.tgz')
+          assetType = 'tar';
+        else if (firstAsset.name.substr(firstAsset.name.length - 4, 4) == '.zip')
+          assetType = 'zip';
+        else
+          return noRelease();
+
+        return hasRelease(firstAsset.url, assetType);
+      }
+    }
+
+    noRelease();
   });
 }
 
@@ -72,32 +116,108 @@ GithubLocation.prototype = {
   download: function(repo, version, hash, outDir, callback, errback) {
     var repoFile = repo.replace('/', '#') + '.git';
 
-    // ensure the output directory exists
-    // and clear the output directory if necessary
-    prepDir(outDir, function(err) {
-      if (err)
-        return errback(err);
+      // check if this version tag has release assets associated with it
+      checkReleases(repo, version, function hasRelease(archiveURL, type) {
+        var inPipe;
 
-      touchRepo(repo, function(notfound) {
+        if (type == 'tar') {
+          inPipe = zlib.createGunzip()
+          .pipe(tar.Extract({ path: outDir }))
+          .on('end', callback);
+        }
+        else if (type == 'zip') {
+          var tmpDir = path.resolve(execOpt.cwd, 'release-' + repo.replace('/', '#') + '-' + version);
+          var tmpFile = tmpDir + '.' + type;
+          if (process.platform.match(/^win/))
+            throw 'No unzip support for windows yet due to https://github.com/nearinfinity/node-unzip/issues/33. Please post a jspm-cli issue.';
+          inPipe = fs.createWriteStream(tmpFile)
+          .on('finish', function() {
+            exec('unzip -o ' + tmpFile + ' -d ' + tmpDir, execOpt, function(err) {
+              if (err)
+                return errback(err);
 
-        if (notfound)
-          return callback();
+              // now rename tmpDir/dist to outDir
+              fs.readdir(tmpDir, function(err, files) {
+                if (err)
+                  return errback(err);
 
-        // do a full download
-        exec('git --git-dir=' + repoFile + ' remote update', execOpt, function(err, stdout, stderr) {
+                if (!files.length || files.length > 1)
+                  return callback();
+
+                clearDir(outDir, function(err) {
+                  if (err)
+                    return errback(err);
+                  fs.rename(path.resolve(tmpDir, files[0]), outDir, function(err) {
+                    if (err)
+                      return errback(err);
+
+                    fs.unlink(tmpFile, function() {
+                      fs.rmdir(tmpDir, callback);
+                    });
+                  });
+                });
+              });
+            });
+          });
+        }
+
+        // has a release archive
+        request({
+          uri: archiveURL, 
+          headers: { 'accept': 'application/octet-stream' },
+          strictSSL: false
+        })
+        .on('response', function(archiveRes) {
+
+          if (archiveRes.statusCode != 200)
+            return errback('Bad response code ' + archiveRes.statusCode);
+          
+          if (archiveRes.headers['content-length'] > 10000000)
+            return errback('Response too large.');
+
+          archiveRes.pause();
+
+          archiveRes.pipe(inPipe);
+
+          archiveRes.on('error', errback);
+
+          archiveRes.resume();
+        })
+        .on('error', errback);
+
+      }, function noRelease() {
+
+        // ensure the output directory exists
+        // and clear the output directory if necessary
+        prepDir(outDir, function(err) {
           if (err)
-            return errback(stderr);
+            return errback(err);
 
-          exec('git --work-tree=' + outDir + ' --git-dir=' + repoFile + ' reset --hard ' + hash, execOpt, function(err, stdout, stderr) {
-            if (err)
-              return errback(stderr);
-            callback();
+          // no release archive
+          touchRepo(repo, function(notfound) {
+
+            if (notfound)
+              return callback();
+
+            // do a full download
+            exec('git --git-dir=' + repoFile + ' remote update', execOpt, function(err, stdout, stderr) {
+              if (err)
+                return errback(stderr);
+
+              exec('git --work-tree=' + outDir + ' --git-dir=' + repoFile + ' reset --hard ' + hash, execOpt, function(err, stdout, stderr) {
+                if (err)
+                  return errback(stderr);
+                callback();
+              });
+
+            });
           });
 
-        });
-      });
 
-    });
+        });
+
+      }, errback);
+
   },
 
   getVersions: function(repo, callback, errback) {
---
+++ test.js
@@ -0,0 +1,13 @@
+var github = require('./github');
+
+github = new github({
+  baseDir: '.',
+  log: true,
+  tmpDir: '.'
+});
+
+github.download('twbs/bootstrap', 'v3.0.0', 'e8a1df5f060bf7e6631554648e0abde150aedbe4', 'test-repo', function() {
+  console.log('done');
+}, function(err) {
+  console.log(err);
+});
