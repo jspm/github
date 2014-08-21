df59b4215fc2cbac997bf574194b8b6664638199
guybedford <guybedford@gmail.com>
2014-08-20 18:54:06 -0700

    endpoint update 0.4.0

--- github.js
+++ github.js
@@ -3,27 +3,21 @@ var fs = require('fs');
 var path = require('path');
 var mkdirp = require('mkdirp');
 var rimraf = require('rimraf');
-var githubAPI = require('github');
-var github = new githubAPI({ version: '3.0.0' });
 var request = require('request');
+
+var Promise = require('rsvp').Promise;
+var asp = require('rsvp').denodeify;
 
 var tar = require('tar');
 var zlib = require('zlib');
 
 var execOpt;
 
-var https;
-
-var log, username, password;
+var username, password;
 var remoteString;
 var apiRemoteString;
-var gitRemoteString;
-
-var isWin = process.platform === 'win32';
 
 var GithubLocation = function(options) {
-  this.baseDir = options.baseDir;
-  log = options.log === false ? false : true;
   username = options.username;
   password = options.password;
   execOpt = {
@@ -31,64 +25,53 @@ var GithubLocation = function(options) {
     timeout: options.timeout * 1000,
     killSignal: 'SIGKILL'
   };
-  https = options.https || false;
-
-  if (isWin)
-    https = true;
 
   remoteString = 'https://' + (username ? (username + ':' + password + '@') : '') + 'github.com/';
-  gitRemoteString = https ? remoteString : 'git://github.com/';
-  apiRemoteString = 'https://' + (username ? (username + ':' + password + '@') : '') + 'api.github.com/repos/';
+  apiRemoteString = 'https://' + (username ? (username + ':' + password + '@') : '') + 'api.github.com/';
 }
 
-var clearDir = function(dir, callback) {
-  fs.exists(dir, function(exists) {
-
-    (exists ? rimraf : function(dir, callback) { callback(); })(dir, function(err) {
-      if (err)
-        return callback(err);
-
-      callback();
-    });
+function clearDir(dir) {
+  return asp(fs.exists)(dir)
+  .then(function(exists) {
+    if (exists)
+      return asp(rimraf)(dir);
   });
 }
 
-var prepDir = function(dir, callback) {
-  clearDir(dir, function(err) {
-    if (err)
-      return callback(err);
-    mkdirp(dir, function(err) {
-      if (err)
-        return callback(err);
-
-      callback();
-    });
-
+function prepDir(dir) {
+  return clearDir(dir)
+  .then(function() {
+    return asp(mkdirp)(dir);
   });
 }
 
-var checkReleases = function(repo, version, hasRelease, noRelease, errback) {
-  if (username)
-    github.authenticate({
-      type: 'basic',
-      username: username,
-      password: password
-    });
-  github.repos.listReleases({
-    user: repo.split('/')[0],
-    repo: repo.split('/')[1]
-  }, function(err, res) {
-    if (err)
-      return errback();
+function checkReleases(repo, version) {
 
+  return asp(request)({
+    uri: apiRemoteString + 'repos' + repo + '/releases',
+    headers: {
+      'User-Agent': 'jspm'
+    },
+    strictSSL: false,
+    followRedirect: false
+  })
+  .then(function(res) {
+    try {
+      return JSON.parse(res.body);
+    }
+    catch(e) {
+      throw 'Unable to parse GitHub API response';
+    }
+  })
+  .then(function(releases) {
     // run through releases list to see if we have this version tag
-    for (var i = 0; i < res.length; i++) {
-      var tagName = res[i].tag_name.trim();
+    for (var i = 0; i < releases.length; i++) {
+      var tagName = releases[i].tag_name.trim();
 
       if (tagName == version) {
-        var firstAsset = res[i].assets[0];
+        var firstAsset = releases[i].assets[0];
         if (!firstAsset)
-          return noRelease();
+          return false;
 
         var assetType;
 
@@ -97,13 +80,12 @@ var checkReleases = function(repo, version, hasRelease, noRelease, errback) {
         else if (firstAsset.name.substr(firstAsset.name.length - 4, 4) == '.zip')
           assetType = 'zip';
         else
-          return noRelease();
+          return false;
 
-        return hasRelease(firstAsset.url, assetType);
+        return { url: firstAsset.url, type: assetType };
       }
     }
-
-    noRelease();
+    return false;
   });
 }
 
@@ -111,129 +93,216 @@ var checkReleases = function(repo, version, hasRelease, noRelease, errback) {
 // check if the given directory contains one directory only
 // so that when we unzip, we should use the inner directory as
 // the directory
-function checkStripDir(dir, callback, errback) {
-  fs.readdir(dir, function(err, files) {
-    if (err)
-      return errback(err);
-
+function checkStripDir(dir) {
+  return asp(fs.readdir)(dir)
+  .then(function(files) {
     if (files.length > 1)
-      return callback(dir);
+      return dir;
 
     var dirPath = path.resolve(dir, files[0]);
 
-    fs.stat(dirPath, function(err, stat) {
-      if (err)
-        return errback(err);
-      if (stat.isDirectory())
-        return callback(dirPath);
-      return callback(dir);
-    });
+    return asp(fs.stat)(dirPath);
+  })
+  .then(function(stat) {
+    if (stat.isDirectory())
+      return dirPath;
+    
+    return dir;
   });
 }
 
 
 GithubLocation.prototype = {
 
-  degree: 2,
+  parse: function(name) {
+    var parts = name.split('/');
+    var packageName = parts.splice(0, 2).join('/');
+    return {
+      package: packageName,
+      path: parts.join('/')
+    };
+  },
+
+  // return values
+  // { versions: { versionhash } }
+  // { redirect: 'newrepo' }
+  // { notfound: true }
+  lookup: function(repo) {
+    return new Promise(function(resolve, reject) {
+
+      var versions, cancel, passed = 0;
+
+      // request the repo to check that it isn't a redirect
+      request({
+        uri: apiRemoteString + 'repos/' + repo,
+        headers: {
+          'User-Agent': 'jspm'
+        },
+        strictSSL: false,
+        followRedirect: false
+      })
+      .on('response', function(res) {
+        if (cancel)
+          return;
+
+        // redirect
+        if (res.statusCode == 301) {
+          cancel = true;
+          return resolve({ redirect: res.headers.location.split('/').splice(3) });
+        }
+        //not found
+        if (res.statusCode == 404) {
+          cancel = true;
+          return resolve({ notfound: true });
+        }
+        //other error
+        else if (res.statusCode != 200) {
+          cancel = true;
+          return reject('Invalid status code ' + res.statusCode);
+        }
+        
+        passed++;
+        if (passed == 2)
+          return resolve({ versions: versions });
+      })
+      .on('error', function(err) {
+        cancel = true;
+        reject(err);
+      });
+
+      exec('git ls-remote ' + remoteString + repo + '.git refs/tags/* refs/heads/*', execOpt, function(err, stdout, stderr) {
+        if (cancel)
+          return;
+
+        if (err) {
+          if ((err + '').indexOf('Repository not found') == -1) {
+            cancel = true;
+            reject(stderr);
+          }
+          return;
+        }
+
+        versions = {};
+        var refs = stdout.split('\n');
+        for (var i = 0; i < refs.length; i++) {
+          if (!refs[i])
+            continue;
+          
+          var hash = refs[i].substr(0, refs[i].indexOf('\t'));
+          var refName = refs[i].substr(hash.length + 1);
+
+          if (refName.substr(0, 11) == 'refs/heads/')
+            versions[refName.substr(11)] = hash;
+          else if (refName.substr(0, 10) == 'refs/tags/') {
+            if (refName.substr(refName.length - 3, 3) == '^{}')
+              versions[refName.substr(10, refName.length - 13)] = hash;
+            else
+              versions[refName.substr(10)] = hash;
+          }
+        }
+
+        passed++;
+        if (passed == 2)
+          resolve({ versions: versions });
+      });
+
+    });
+  },
+
+  // optional hook, allows for quicker dependency resolution
+  // since download doesn't block dependencies
+  loadConfig: function(repo, version, hash) {
+    // NB ensure this works for private repos
+    var reqOptions = {
+      uri: 'https://raw.githubusercontent.com/' + repo + '/' + hash + '/package.json',
+      strictSSL: false
+    };
+    if (username)
+      reqOptions.auth = {
+        user: username,
+        pass: password
+      };
+    
+    return asp(request)(reqOptions).then(function(res) {
+      if (res.statusCode == 404) {
+        // it is quite valid for a repo not to have a package.json
+        return {};
+      }
+      if (res.statusCode != 200)
+        throw 'Unable to check repo package.json for release';
+      
+      var packageJSON;
+
+      try {
+        packageJSON = JSON.parse(res.body);
+      }
+      catch(e) {
+        throw 'Error parsing package.json';
+      }
+
+      return packageJSON;
+    });
+  },
 
   // always an exact version
   // assumed that this is run after getVersions so the repo exists
-  download: function(repo, version, hash, outDir, callback, errback) {
-    var _errback = errback;
-    errback = function(err) {
-      return _errback(err);
-    }
-    if (log)
-      console.log(new Date() + ': Requesting package github:' + repo);
+  download: function(repo, version, hash, outDir) {
+    
+    return checkReleases(repo, version)
+    .then(function(release) {
+      if (!release)
+        return true;
 
-      // check if this version tag has release assets associated with it
-      checkReleases(repo, version, function hasRelease(archiveURL, type) {
+      // Download from the release archive
+      return new Promise(function(resolve, reject) {
+
         var inPipe;
 
-        var downloaded, packageJSON;
-        var complete = function() {
-          if (downloaded && packageJSON)
-            callback(packageJSON);
-        }
-
-        if (type == 'tar') {
+        if (release.type == 'tar') {
           inPipe = zlib.createGunzip()
           .pipe(tar.Extract({ path: outDir, strip: 1 }))
           .on('end', function() {
-            downloaded = true;
-            complete();
-          });
+            resolve();
+          })
+          .on('error', reject);
         }
-        else if (type == 'zip') {
+        else if (release.type == 'zip') {
           var tmpDir = path.resolve(execOpt.cwd, 'release-' + repo.replace('/', '#') + '-' + version);
           var tmpFile = tmpDir + '.' + type;
           if (process.platform.match(/^win/))
             return errback('No unzip support for windows yet due to https://github.com/nearinfinity/node-unzip/issues/33. Please post a jspm-cli issue.');
+          
           inPipe = fs.createWriteStream(tmpFile)
           .on('finish', function() {
-            exec('unzip -o ' + tmpFile + ' -d ' + tmpDir + ' && chmod -R +w ' + tmpDir, execOpt, function(err) {
-              if (err)
-                return errback(err);
-
-              // check if it extracted to a single dir and use that
-              checkStripDir(tmpDir, function(repoDir) {
-                // now rename tmpDir/dist to outDir
-                prepDir(outDir, function(err) {
-                  if (err)
-                    return errback(err);
-                  fs.rename(repoDir, outDir, function(err) {
-                    if (err)
-                      return errback(err);
-
-                    // if it was a subdir, remove the base dir too
-                    if (repoDir != tmpDir)
-                      fs.rmdirSync(tmpDir);
-
-                    fs.unlink(tmpFile, function() {
-                      downloaded = true;
-                      complete();
-                    });
-                  });
-                });
-              }, errback);
-            });
-          });
+            Promise.resolve()
+            .then(function() {
+              return asp(exec)('unzip -o ' + tmpFile + ' -d ' + tmpDir + ' && chmod -R +w ' + tmpDir, execOpt)
+            })
+            .then(function() {
+              return checkStripDir(tmpDir);
+            })
+            .then(function(repoDir) {
+              return prepDir(outDir);
+            })
+            .then(function() {
+              return asp(fs.rename)(repoDir, outDir);
+            })
+            .then(function() {
+              if (repoDir != tmpDir)
+                return asp(fs.rmdir)(tmpDir);
+            })
+            .then(function() {
+              return asp(fs.unlink)(tmpFile);
+            })
+            .then(resolve, reject);
+          })
+          .on('error', reject);
         }
         else {
-          return errback('Github release found, but no archive present.');
+          throw 'Github release found, but no archive present.';
         }
 
-        // in parallel, check the underlying repo for a package.json
-
-        // NB what are the implications of this for private repos?
-        var reqOptions = {
-          uri: 'https://raw.githubusercontent.com/' + repo + '/' + hash + '/package.json',
-          strictSSL: false
-        };
-        if (username)
-          reqOptions.auth = {
-            user: username,
-            pass: password
-          };
-        
-        request(reqOptions, function(err, res, body) {
-          if (res.statusCode == 404) {
-            packageJSON = {};
-            return complete();
-          }
-          if (err || res.statusCode != 200)
-            return errback('Unable to check repo package.json for release');
-          try {
-            packageJSON = JSON.parse(body);
-          }
-          catch(e) {
-            return errback('Error parsing package.json');
-          }
-
-          complete();
-        });
-
-        // has a release archive
+        // now that the inPipe is ready, do the request
         request({
           uri: archiveURL, 
           headers: { 
@@ -243,23 +312,28 @@ GithubLocation.prototype = {
           strictSSL: false
         }).on('response', function(archiveRes) {
           if (archiveRes.statusCode != 200)
-            return errback('Bad response code ' + archiveRes.statusCode + '\n' + JSON.sringify(archiveRes.headers));
+            return reject('Bad response code ' + archiveRes.statusCode + '\n' + JSON.sringify(archiveRes.headers));
           
           if (archiveRes.headers['content-length'] > 100000000)
-            return errback('Response too large.');
+            return reject('Response too large.');
 
           archiveRes.pause();
 
           archiveRes.pipe(inPipe);
 
-          archiveRes.on('error', errback);
+          archiveRes.on('error', reject);
 
           archiveRes.resume();
         })
-        .on('error', errback);
+        .on('error', reject);
+      });
+    })
+    .then(function(git) {
+      if (!git)
+        return;
 
-      }, function noRelease() {
-
+      // Download from the git archive
+      return new Promise(function(resolve, reject) {
         request({
           uri: remoteString + repo + '/archive/' + version + '.tar.gz',
           headers: { 'accept': 'application/octet-stream' },
@@ -268,10 +342,10 @@ GithubLocation.prototype = {
         .on('response', function(pkgRes) {
 
           if (pkgRes.statusCode != 200)
-            return errback('Bad response code ' + pkgRes.statusCode);
+            return reject('Bad response code ' + pkgRes.statusCode);
           
           if (pkgRes.headers['content-length'] > 10000000)
-            return errback('Response too large.');
+            return reject('Response too large.');
 
           pkgRes.pause();
 
@@ -280,88 +354,14 @@ GithubLocation.prototype = {
           pkgRes
           .pipe(gzip)
           .pipe(tar.Extract({ path: outDir, strip: 1 }))
-          .on('error', errback)
-          .on('end', callback);
+          .on('error', reject)
+          .on('end', resolve);
 
           pkgRes.resume();
 
         })
-        .on('error', errback);
-
-      }, errback);
-
-  },
-
-  getVersions: function(repo, callback, errback) {
-
-    var passed = 0;
-    var error = false;
-    var versions;
-
-    // request the repo to check that it isn't a redirect!
-    request({
-      uri: apiRemoteString + repo,
-      headers: {
-          'User-Agent': "jspm"
-      },
-      strictSSL: false,
-      followRedirect: false
-    })
-    .on('response', function(res) {
-      //not found
-      if (res.statusCode == 301 || res.statusCode == 404) {
-        error = true;
-        return callback();
-      }
-      //other error
-      else if (res.statusCode != 200) {
-        error = true;
-        return errback(res.statusCode);
-      }
-      if (error)
-        return;
-      
-      passed++;
-
-      if (passed == 2)
-        callback(versions);
-    })
-    .on('error', function(err) {
-      error = true;
-      errback(err);
-    });
-
-    exec('git ls-remote ' + gitRemoteString + repo + '.git refs/tags/* refs/heads/*', execOpt, function(err, stdout, stderr) {
-
-      if (err) {
-        error = true;
-        if ((err + '').indexOf('Repository not found') != -1)
-          return callback();
-        return errback(stderr);
-      }
-
-      versions = {};
-      var refs = stdout.split('\n');
-      for (var i = 0; i < refs.length; i++) {
-        if (!refs[i])
-          continue;
-        
-        var hash = refs[i].substr(0, refs[i].indexOf('\t'));
-        var refName = refs[i].substr(hash.length + 1);
-
-        if (refName.substr(0, 11) == 'refs/heads/')
-          versions[refName.substr(11)] = hash;
-        else if (refName.substr(0, 10) == 'refs/tags/') {
-          if (refName.substr(refName.length - 3, 3) == '^{}')
-            versions[refName.substr(10, refName.length - 13)] = hash;
-          else
-            versions[refName.substr(10)] = hash;
-        }
-      }
-
-      passed++;
-      if (passed == 2)
-        callback(versions);
+        .on('error', reject);
+      });
     });
   }
 };
