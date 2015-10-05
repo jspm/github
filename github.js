9d566b0b850b3df7b012ed97f9beaee922d715ae
guybedford <guybedford@gmail.com>
2015-10-05 15:07:11 +0200

    only set any main if found

--- github.js
+++ github.js
@@ -815,7 +815,7 @@ GithubLocation.prototype = {
     return checkMain(main, libDir)
     .then(function(hasMain) {
       if (hasMain)
-        return;
+        return hasMain;
 
       return asp(fs.readFile)(path.resolve(dir, 'bower.json'))
       .then(function(bowerJson) {
@@ -833,11 +833,17 @@ GithubLocation.prototype = {
         return checkMain(main);
       }, function() {})
       .then(function(hasBowerMain) {
-        if (!hasBowerMain)
-          return;
+        if (hasBowerMain)
+          return hasBrowserMain;
 
-        pjson.main = main;
+        main = 'index';
+        return checkMain(main, libDir);
       });
+    })
+    .then(function(hasMain) {
+      if (hasMain)
+        packageConfig.main = main;
+      return packageConfig;
     });
   }
 
